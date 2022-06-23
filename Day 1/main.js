c5f61140a7d6427d5da80dc035345a04722e77d0
guimelganu <guimelgn96@hotmail.com>
2022-06-23 18:00:09 -0500

    Notes

---
+++ Day 1/main.js
@@ -0,0 +1,18 @@
+let nombre = 'Jorge',
+    apellido = 'Garcia',
+    pais = "México",
+    ciudad = "Estado de México",
+    edad = 25,
+    casado = false;
+
+    console.log(nombre, apellido, pais, ciudad, edad, casado);
+
+//Declaramos variables en una misma linea y las manamos a llamar.
+
+const gravedad = 9.81,
+          puntoEbullicion = 100,
+          pi = 3.14;
+
+    console.log(gravedad, puntoEbullicion, pi);
+
+//Declaramos constantes en una misma linea y las mandamos a llamar.
