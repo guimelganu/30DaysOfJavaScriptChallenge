0bf7413bbd68f43a074b26085c2c5f91878c974d
guimelganu <guimelgn96@hotmail.com>
2022-06-23 18:18:15 -0500

    Resolved Exercises

--- Day 1/Exercises/variable.js
+++ Day 1/Exercises/variable.js
@@ -2,3 +2,16 @@ let saludo = "Hola esto es un saludo";
 let hombre = true;
 let nombre;
 let nada = null;
+
+let nombre2;
+let apellido;
+let estadoCivil;
+let pais;
+let edad;
+
+let nombre3, apellido2, estadoCivil2, pais2, edad2;
+
+let miEdad = 25;
+let tuEdad = 23;
+
+console.log("Mi edad es: " + miEdad + " Tu edad es: " + tuEdad);
